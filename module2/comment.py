8c51f4c0e152a7b15e4b43da866ad689c93edbcb
Pedro Luis <pedroluisnr22@gmail.com>
2025-03-31 12:32:24 -0500

    fet: :sparkles: finish module 2

---
+++ module2/comment.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+from .post import UserPost
+
+
+class CommentIn(BaseModel):
+    body: str
+    post_id: int
+
+
+class Comment(CommentIn):
+    id: int
+
+
+class UserPostWithComments(BaseModel):
+    post: UserPost
+    comments: list[Comment]
---
+++ module2/router.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, HTTPException
+
+from .comment import Comment, CommentIn, UserPostWithComments
+from .post import UserPost, UserPostIn
+
+postRouter = APIRouter(prefix="/posts")
+commentRouter = APIRouter(prefix="/comments")
+
+post_table = {}
+comments_table = {}
+
+
+def find_post(post_id: int):
+    return post_table.get(post_id)
+
+
+@postRouter.post("", response_model=UserPost, status_code=201)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()  # previously .dict()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@postRouter.get("", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
+
+
+@postRouter.get("/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return {
+        "post": post,
+        "comments": await get_comments_on_post(post_id),
+    }
+
+
+@postRouter.get("/{post_id}/comments", response_model=list[Comment])
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comments_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@commentRouter.post("", response_model=Comment, status_code=201)
+async def create_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    data = comment.model_dump()  # previously .dict()
+    last_record_id = len(comments_table)
+    new_comment = {**data, "id": last_record_id}
+    comments_table[last_record_id] = new_comment
+    return new_comment
